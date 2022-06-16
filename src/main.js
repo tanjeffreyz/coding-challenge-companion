9b9b760460d7230633a76b94e104a8f38cd25e66
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-06-15 23:46:59 -0700

    added skeleton for commit file

--- src/main.js
+++ src/main.js
@@ -7,19 +7,41 @@ KEYS = [
 ];
 
 
+function clearStorage() {
+    for (let i = 0; i < KEYS.length; i++) {
+        const dict = {};
+        dict[KEYS[i]] = null;
+        chrome.storage.local.set(dict, () => {});
+    }
+}
+
+
+function noNullMembers(obj) {
+    for (let member in target) {
+        if (obj[member] === null) return true;
+    }
+    return false;
+}
+
+
 chrome.runtime.onMessage.addListener((message) => {
     let response = null;
     switch(message.type) {
         case 'clear-storage':
-            for (let i = 0; i < KEYS.length; i++) {
-                const dict = {};
-                dict[KEYS[i]] = null;
-                chrome.storage.local.set(dict, () => {});
-            }
+            clearStorage();
             break;
         case 'commit-file':
-            const name = message.name;
-            // chrome.storage.
+            chrome.storage.local.get(
+                ['name', 'login', 'email', 'repository'],
+                (data) => {
+                    if (data && noNullMembers(data)) {
+                        const url = `https://api.github.com/repos/${data.login}/${data.repository}/${message.path}`;
+                        
+                    } else {
+                        clearStorage();
+                    }
+                }
+            );
             break;
     }
     message.reponse = response;
